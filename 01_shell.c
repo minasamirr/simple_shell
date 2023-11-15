ac3111dc650d15df0762166d4bb4421da96f4fdb
minasamirr <1701538@eng.asu.edu.eg>
2023-11-15 08:54:59 -0800

    01

--- 01_shell.c
+++ 01_shell.c
@@ -6,8 +6,6 @@
 #include <sys/wait.h>
 
 #define MAX_INPUT_SIZE 1024
-#define MAX_ARG_SIZE 64
-#define MAX_ARGS 16
 
 void display_prompt() {
     printf("#cisfun$ ");
@@ -16,13 +14,10 @@ void display_prompt() {
 
 int main() {
     char input[MAX_INPUT_SIZE];
-    char *args[MAX_ARGS];
     pid_t pid;
-    char *token;
 
     while (1) {
-	int i;
-	    display_prompt();
+        display_prompt();
 
         if (fgets(input, MAX_INPUT_SIZE, stdin) == NULL) {
             printf("\n");
@@ -31,23 +26,13 @@ int main() {
 
         input[strcspn(input, "\n")] = '\0';
 
-        token = strtok(input, " ");
-        i = 0;
-
-        while (token != NULL && i < MAX_ARGS - 1) {
-            args[i++] = token;
-            token = strtok(NULL, " ");
-        }
-
-        args[i] = NULL;
-
         pid = fork();
 
         if (pid == -1) {
             perror("fork");
             exit(EXIT_FAILURE);
         } else if (pid == 0) {
-            if (execvp(args[0], args) == -1) {
+            if (execlp(input, input, (char *)NULL) == -1) {
                 perror("exec");
                 exit(EXIT_FAILURE);
             }
@@ -56,10 +41,11 @@ int main() {
             waitpid(pid, &status, 0);
 
             if (WIFEXITED(status) && WEXITSTATUS(status) != 0) {
-                fprintf(stderr, "./shell: %s: command not found\n", args[0]);
+                fprintf(stderr, "./shell: %s: command not found\n", input);
             }
         }
     }
 
     return 0;
 }
+
